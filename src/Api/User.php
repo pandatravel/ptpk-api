2b530ffdb827a9337cc4a7a9d13c8cccda78fa7d
Ammon Casey <ammon@caseyohana.com>
2018-01-26 14:57:04 -1000

    [api] refactored User endpoint

--- src/Api/User.php
+++ src/Api/User.php
@@ -2,8 +2,6 @@
 
 namespace Ammonkc\Ptpkg\Api;
 
-use Ammonkc\Ptpkg\Exception\MissingArgumentException;
-
 /**
  * Listing users, showing, and updating
  *
@@ -29,20 +27,6 @@ class User extends AbstractApi
     }
 
     /**
-     * List users
-     *
-     * @link https://ptpkg.dev/api/v1/users/
-     *
-     * @param array  $params     the additional parameters
-     *
-     * @return array list of users found
-     */
-    public function all(array $params = [])
-    {
-        return $this->get($this->getEndpoint(), array_merge(['page' => 1], $params));
-    }
-
-    /**
      * Get extended information about a tour by its id
      *
      * @link https://ptpkg.dev/api/v1/user/
@@ -54,36 +38,6 @@ class User extends AbstractApi
     public function me()
     {
         return $this->get($this->getEndpoint());
-    }
-
-    /**
-     * Get extended information about a tour by its id
-     *
-     * @link https://ptpkg.dev/api/v1/users/
-     *
-     * @param int    $id         the tour number
-     *
-     * @return array information about the tour
-     */
-    public function show(int $id)
-    {
-        return $this->get($this->getEndpoint() . '/' . rawurlencode($id));
-    }
-
-    /**
-     * Create a new tour
-     *
-     * @link https://ptpkg.dev/api/v1/users/
-     *
-     * @param array  $params     the new tour data
-     *
-     * @throws MissingArgumentException
-     *
-     * @return array information about the tour
-     */
-    public function create(array $params)
-    {
-        return $this->post($this->getEndpoint(), $params);
     }
 
     /**
